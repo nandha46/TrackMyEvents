301fe937250ff46e56e5a37b1720d6a6a2cb523a
Nandha <nandha.kumar790@gmail.com>
2024-10-16 19:20:22 +0530

    code updates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,42 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="mt-10 grid gap-2 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+                        <div class="gradient timecard">
+                          <div class="text-wrapper">
+                            <p class="text-4xl text-white custom-font-style">Acer Aspire 7</p>
+                            <p class="text-4xl text-white custom-font-style">4Y 3M 114D </p>
+                            <p class="text-3xl text-white custom-font-style">&LeftAngleBracket;Since&RightAngleBracket;</p>
+                          </div>
+                        </div>
+                        <div class="gradient2 timecard">
+                          <div class="text-wrapper">
+                          <p class="text-4xl text-white custom-font-style">SKAT Tech Join</p>
+                          <p class="text-4xl text-white custom-font-style">4Y 3M 114D </p>
+                          <p class="text-3xl text-white custom-font-style">&LeftAngleBracket;Since&RightAngleBracket;</p>
+                          </div>
+                        </div>
+                        <div class="gradient timecard">
+                          <div class="text-wrapper">
+                          <p class="text-4xl text-white custom-font-style">SKAT Tech Last Day</p>
+                          <p class="text-4xl text-white custom-font-style">4Y 3M 114D </p>
+                          <p class="text-3xl text-white custom-font-style">&LeftAngleBracket;Since&RightAngleBracket;</p>
+                          </div>
+                        </div>
+                        <div class="timecard bg-1">
+                          <div class="text-wrapper">
+                            <p class="text-4xl text-white custom-font-style">Samsung S21 FE</p>
+                            <p class="text-4xl text-white custom-font-style">1Y 163D 12H57M03S </p>
+                            <p class="text-3xl text-white custom-font-style">&LeftAngleBracket;Since&RightAngleBracket;</p>
+                            </div>
+                        </div>
+                        <div class="gradient timecard bg-2"></div>
+                        <div class="gradient2 timecard bg-3"></div>
+                        <div class="gradient timecard bg-4"></div>
+                        <div class="gradient timecard"></div>
+                        <div class="gradient timecard"></div>
                 </div>
             </div>
         </div>
