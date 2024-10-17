043f0bfde16e3adf80800ed14113038ff57fe8bf
Nandha <nandha.kumar790@gmail.com>
2024-10-17 19:13:10 +0530

    modal form added
    fixed alpine js click events

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,9 +1,11 @@
-<nav class="bg-gray-800">
+<nav class="bg-gray-800" x-data="{ mobileMenuOpen: false, profileMenuOpen: false }">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         <!-- Mobile menu button-->
         <button type="button"
+          class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          @click="mobileMenuOpen = !mobileMenuOpen" type="button"
           class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           aria-controls="mobile-menu" aria-expanded="false">
           <span class="absolute -inset-0.5"></span>
@@ -49,14 +51,28 @@
           </div>
         </div>
       </div>
-
-      {{-- new page copy --}}
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <div style="margin-right: 1rem">
-          <button class="button custom-btn">
+        <div class="mr-8">
+          <label class="relative block">
+            <span class="sr-only">Search</span>
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">
+                <path fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </span>
+            <input
+              class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Search for anything..." type="text" name="search" />
+          </label>
+        </div>
+        <div x-data="{open: false}" class="mr-4" x-cloak>
+          <button @click=" open =! open; console.log(open);" class="button custom-btn">
             <span>+</span>
             New Event
           </button>
+          <x-modals.form />
         </div>
         <button type="button"
           class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -71,9 +87,9 @@
         </button>
 
         <!-- Profile dropdown -->
-        <div class="relative ml-3">
+        <div class="relative ml-3" x-data="{ profileMenuOpen: false }">
           <div>
-            <button type="button"
+            <button type="button" @click="profileMenuOpen = !profileMenuOpen"
               class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               id="user-menu-button">
               <span class="absolute -inset-1.5"></span>
@@ -82,22 +98,31 @@
             </button>
           </div>
 
-          <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" style="display: none;">
-            <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-              id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700"
-              role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+          <div x-show="profileMenuOpen" @click.away="profileMenuOpen = false" 
+            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            x-transition:enter="transition ease-out duration-100"
+            x-transition:enter-start="transform opacity-0 scale-95"
+            x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="transform opacity-100 scale-100"
+            x-transition:leave-end="transform opacity-0 scale-95"
+            >
+            <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
+              tabindex="-1" id="user-menu-item-0">Your Profile</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+              id="user-menu-item-1">Settings</a>
             <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700">Sign out</a>
           </div>
 
         </div>
       </div>
-      {{-- new page copy end --}}
     </div>
   </div>
 
   <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="sm:hidden" id="mobile-menu">
+  <div class="sm:hidden" id="mobile-menu" x-show="mobileMenuOpen" @click.away="mobileMenuOpen = false"
+  x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform scale-95"
+  x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-200"
+  x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-95">
     <div class="space-y-1 px-2 pb-3 pt-2">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
